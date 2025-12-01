5a805036a4e1a89c18bd316cb2e828f89c21852b
Victor <victorbarbosalima726@gmail.com>
2025-12-01 17:11:24 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 function cardflip() {
   const virar = document.querySelector(".popup-notificacao");
+  virar.classList.remove("alterar");
+
   virar.classList.add("alterar");
+
   console.log("está funcionando");
+}
+
+function flipResposta() {
+  const virar = document.querySelector(".popup-respostas");
+  virar.classList.add("popup-aparecer");
 }
